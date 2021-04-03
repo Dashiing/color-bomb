76987f74398b69cbdb4043150828732364170601
Rui Lopes <ruipcl@gmail.com>
2021-04-03 16:04:33 +0100

    created nx workspace

---
+++ apps/color-bomb/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'color-bomb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'color-bomb';
+}
