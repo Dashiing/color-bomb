f1d9a27c9226eea5a3e4a905330e88c3f54afc98
Rui Lopes <ruipcl@gmail.com>
2021-04-04 16:05:59 +0100

    Implemented swap the colors of bins every 40 seconds

--- apps/color-bomb/src/app/app.module.ts
+++ apps/color-bomb/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
 import { BinComponent } from './components/bin/bin.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BombComponent } from './components/bomb/bomb.component';
 
 @NgModule({
-  declarations: [AppComponent, BoardComponent, BinComponent],
-  imports: [BrowserModule, DragDropModule],
+  declarations: [AppComponent, BoardComponent, BinComponent, BombComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/color-bomb/src/app/components/board/board.component.ts
+++ apps/color-bomb/src/app/components/board/board.component.ts
@@ -1,4 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { BoardService } from './board.service';
 
 @Component({
   selector: 'cb-board',
@@ -6,4 +9,17 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class BoardComponent {}
+export class BoardComponent {
+  binColors$: Observable<string[]>;
+  colors = ['red', 'blue', 'green'];
+
+  colorSwitchIntervalPeriod = 40000;  // milliseconds
+
+  constructor(boardService: BoardService) {
+    this.binColors$ = interval(this.colorSwitchIntervalPeriod)
+      .pipe(
+        map(() => boardService.shuffle(this.colors)),
+        startWith(this.colors)
+      )
+  }
+}
